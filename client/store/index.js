c225f0a86743d86f20733464b30d0f14fb2d42b1
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-12 19:24:17 -0500

    chore: set-up media reducer

--- client/store/index.js
+++ client/store/index.js
@@ -2,13 +2,17 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import  reduce  from './reducers/reduce'
+import  mediaReducer from './reducers/media'
 
 
-const reducer = combineReducers({reduce})
+const reducer = combineReducers({
+  mediaReducer,
+});
+
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+
+const store = createStore(reducer, middleware);
 
 export default store;
---
+++ client/store/reducers/media.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const initialState = {
+  media: [],
+};
+
+// ACTION TYPES
+const GOT_MEDIA = 'GOT_MEDIA';
+
+
+// ACTION CREATORS
+export const gotMedia = media => {
+  return {
+    type: GOT_MEDIA, 
+    media
+  }
+}
+
+// THUNK
+export const getMediaThunk = () => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get('/api/media');
+      dispatch(gotMedia(data));
+    } catch (error) {
+      console.log('This error is at getMediaThunk ', error);
+    }
+  }
+
+}
+
+// MEDIA REDUCER
+const mediaReducer = (state = initialState, action) => { 
+  switch(action.type) {
+    case GOT_MEDIA: 
+      return {...state, media: action.media };
+    default:
+      return state;
+  }
+}
+
+export default mediaReducer;
