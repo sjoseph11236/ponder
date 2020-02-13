e7bfd9418269d8fc75391dc1826ee64a68b2a1ba
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-13 18:32:40 -0500

    feat: create gotCombo Thunk

--- client/store/reducers/media.js
+++ client/store/reducers/media.js
@@ -2,17 +2,25 @@ import axios from 'axios';
 
 const initialState = {
   media: [],
+  combo: [],
 };
 
 // ACTION TYPES
 const GOT_MEDIA = 'GOT_MEDIA';
-
+const GOT_COMBO = 'GOT_COMBO';
 
 // ACTION CREATORS
 export const gotMedia = media => {
   return {
     type: GOT_MEDIA, 
     media
+  }
+}
+
+export const gotCombo = combo => { 
+  return { 
+    type: GOT_COMBO,
+    combo
   }
 }
 
@@ -23,17 +31,29 @@ export const getMediaThunk = () => {
       const { data } = await axios.get('/api/media'); 
       dispatch(gotMedia(data));
     } catch (error) {
-      console.log('This error is at getMediaThunk ', error);
+      console.error('This error is at getMediaThunk ', error);
     }
   }
+}
 
+export const getComboThunk = word => { 
+  return async dispatch => {
+    try {
+      const { data } = await axios.get(`/api/media/combo/${word}`);
+      dispatch(gotCombo(data));
+    } catch (error) {
+      console.error('This error is at getComboThunk ', error);
+    }
+  }
 }
 
 // MEDIA REDUCER
 const mediaReducer = (state = initialState, action) => { 
   switch(action.type) {
     case GOT_MEDIA: 
-      return {...state, media: action.media };
+      return { ...state, media: action.media };
+    case GOT_COMBO: 
+      return { ...state, combo: action.combo };
     default:
       return state;
   }
