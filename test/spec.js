107ad87853e875c0554c21fc65bcedf40f0bdde4
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-18 22:03:01 -0500

    feat: add test for class Method

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const db = require('./test_db');
 const { red } = require('chalk')
+const { Media } = require('./test_db');
 
 describe('Ponder TDD ', () => {
   describe('Models', () => {
@@ -15,17 +16,27 @@ describe('Ponder TDD ', () => {
       
       it('All media should have a title', ()=> {
         expect(seed.media.medium1.title).to.equal('Coming to America');
-        expect(seed.media.medium2.title).to.equal('American Psycho');
-        expect(seed.media.medium3.title).to.equal('This is America');
-        expect(seed.media.medium4.title).to.equal('Us');
+        expect(seed.media.medium2.title).to.equal('This is America');
+        expect(seed.media.medium3.title).to.equal('Us');
+        expect(seed.media.medium4.title).to.equal('American Psycho');
       });
 
       it('All media should have an artist', ()=> {
         expect(seed.media.medium1.artist).to.equal('John Landis');
-        expect(seed.media.medium2.artist).to.equal('Mary Harron');
-        expect(seed.media.medium3.artist).to.equal('Donald Glover');
-        expect(seed.media.medium4.artist).to.equal('Jordan Peele');
+        expect(seed.media.medium2.artist).to.equal('Donald Glover');
+        expect(seed.media.medium3.artist).to.equal('Jordan Peele');
+        expect(seed.media.medium4.artist).to.equal('Mary Harron');
       });
+
+      describe('filterByKeyword', () => {
+        it('returns array with media with the keyword ', async ()=> {
+          const filtered  = await Media.filterByKeyword('america');
+          expect(filtered.length).to.equal(3);
+        });
+      })
+      
     });
+
+    
   });
 });
--- test/test_db.js
+++ test/test_db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
-// const { Media } = require('../server/db/models/media');
+const { Op } = Sequelize;
+// const  Media  = require('../server/db/models/media');
 const chalk = require('chalk');
 const {green, red,  yellow} = require('chalk')
 console.log(yellow('Opening database connection'));
@@ -8,6 +9,23 @@ const db = new Sequelize('postgres://localhost:5432/ponder_tdd_db', {
   logging: false
 });
 
+
+// const Media = db.define('media', { 
+//   title: { 
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     validate: { 
+//       notEmpty: true
+//     }
+//   }, 
+//   artist: { 
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     validate: { 
+//       notEmpty: true
+//     }
+//   }, 
+// });
 
 const Media = db.define('media', { 
   title: { 
@@ -24,20 +42,76 @@ const Media = db.define('media', {
       notEmpty: true
     }
   }, 
+  description: { 
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: { 
+      notEmpty: true
+    }
+  }, 
+  url:{ 
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: { 
+      notEmpty: true
+    }
+  },
+  imageUrl:{ 
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: { 
+      notEmpty: true
+    },
+    defaultValue: 'https://amerikicklanghorne.com/wp-content/uploads/2017/04/default-image.jpg'
+  }
 });
 
+Media.filterByKeyword = async (keyword) => {
+  try {
+    const filteredByKeywordInDescription = await Media.findAll({
+      where: { 
+        description: {
+          // Op.iLike makes the keyword case insentive
+          [Op.iLike]: `%${keyword}%`
+        }
+      }
+    })
+
+    return filteredByKeywordInDescription;
+
+  } catch (error) {
+    console.log('This error is coming from class method in filterByKeyword ', error );
+  }
+};
+
 const media = [
-  { title: "Coming to America",
+  {
+    title: "Coming to America", 
     artist: "John Landis", 
+    description: "Prince Akeem (Eddie Murphy) is the prince of a wealthy African country and wants for nothing, except a wife who will love him in spite of his title. To escape an arranged marriage, Akeem flees to America accompanied by his persnickety sidekick, Semmi (Arsenio Hall), to find his queen. Disguised as a foreign student working in fast food, he romances Lisa (Shari Headley), but struggles with revealing his true identity to her and his marital intentions to his king father (James Earl Jones).",
+    url: "https://www.youtube.com/embed/IZQFJ6hZNJc",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/3/38/ComingtoAmerica1988MoviePoster.jpg/220px-ComingtoAmerica1988MoviePoster.jpg"
   },
-  { title: "American Psycho",
-    artist: "Mary Harron",
-  },
-  { title: "This is America",
+  {
+    title: "This is America", 
     artist: "Donald Glover", 
+    description: "A song by American rapper Donald Glover, under his musical stage name Childish Gambino. Written by Glover, Ludwig Göransson, and Jeffery Lamar Williams, and produced by Glover and Göransson, it was released on May 5, 2018, at the same time that Gambino was hosting an episode of Saturday Night Live. The song features background vocals by American rappers Young Thug (who also has writing credit as Williams), Slim Jxmmi of Rae Sremmurd, BlocBoy JB, Quavo of Migos, and 21 Savage. The song addresses the wider issue of gun violence in the United States, the high rate of mass shootings in the United States, along with longstanding racism and discrimination against African Americans.",
+    url: "https://www.youtube.com/embed/VYOjWnS4cMY",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/This_Is_America_%28single_cover%29_2018.jpg/220px-This_Is_America_%28single_cover%29_2018.jpg"
   },
-  { title: "Us",
+  {
+    title: "Us", 
     artist: "Jordan Peele", 
+    description: "Accompanied by her husband, son and daughter, Adelaide Wilson returns to the beachfront home where she grew up as a child. Haunted by a traumatic experience from the past, Adelaide grows increasingly concerned that something bad is going to happen. Her worst fears soon become a reality when four masked strangers descend upon the house, forcing the Wilsons into a fight for survival. When the masks come off, the family is horrified to learn that each attacker takes the appearance of one of them.",
+    url: "https://www.youtube.com/embed/vh7_WKODlE8",
+    imageUrl: "http://t2.gstatic.com/images?q=tbn:ANd9GcRsuQJNsOm5Gt-apl6_1KSYupoMvgA9R2kl5ZEKIixEJ_HVQ62d"
+  },
+  {
+    title: "American Psycho", 
+    artist: "Mary Harron", 
+    description: "The movie American Psycho is set in New York City in 1987, a handsome, young urban professional, Patrick Bateman (Christian Bale), lives a second life as a gruesome serial killer by night. The cast is filled by the detective (Willem Dafoe), the fiance (Reese Witherspoon), the mistress (Samantha Mathis), the coworker (Jared Leto), and the secretary (Chloë Sevigny). This is a biting, wry comedy examining the elements that make a man a monster.",
+    url: "https://www.youtube.com/embed/RjKNbfA64EE",
+    imageUrl: "https://www.gstatic.com/tv/thumb/v22vodart/24116/p24116_v_v8_ax.jpg"
   },
 ]
 
@@ -61,5 +135,6 @@ const syncAndSeed = async () => {
 };
 
 module.exports = { 
-  syncAndSeed
+  syncAndSeed,
+  Media
 }
