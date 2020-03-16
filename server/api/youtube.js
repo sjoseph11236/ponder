40fd1164eeb5bf16a8b2ecf93182176f28aceade
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-03-16 13:12:52 -0400

    feat: get music data with youtube api

--- server/api/youtube.js
+++ server/api/youtube.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const axios = require('axios');
 const API = process.env.API_TOKEN || require('../../secret');
 
-
+// Max Results: 
 const max = 50;
 
 router.get('/movies/:q', async (req, res, next) => { 
@@ -11,6 +11,19 @@ router.get('/movies/:q', async (req, res, next) => {
     const channelID = 'UC3gNmTGu-TTbFPpfSs5kNkg';
     const q = req.params.q;
     const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&maxResults=${max}&q=${q}&type=video&key=${API}`;
+    const { data } = await axios.get(URL);
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+
+router.get('/music/:q', async (req, res, next) => { 
+  try {
+    const q = req.params.q;
+    const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${max}&q=${q}&type=video&key=${API}`;
     const { data } = await axios.get(URL);
     res.json(data);
   } catch (error) {
